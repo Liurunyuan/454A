4e07a2207bf1d8ec1bfcd44dfcef2ba3006ce283
Simon <liurunyuan0802@163.com>
2020-02-29 22:23:10 +0800

    set sci working mode and fifo configuration

--- platform/pfal/cfg/src/pf_sci_cfg.c
+++ platform/pfal/cfg/src/pf_sci_cfg.c
@@ -41,14 +41,16 @@ void CFG_SCI_A(CFG_SCI_TBL cfgSciTblElement)
     EALLOW;
     GpioCtrlRegs.GPAPUD.bit.GPIO28 = 0;    // Enable pull-up for GPIO28 (SCIRXDA)
     GpioCtrlRegs.GPAPUD.bit.GPIO29 = 0;    // Enable pull-up for GPIO29 (SCITXDA)
-
     GpioCtrlRegs.GPAQSEL2.bit.GPIO28 = 3;  // Asynch input GPIO28 (SCIRXDA)
-
     GpioCtrlRegs.GPAMUX2.bit.GPIO28 = 1;   // Configure GPIO28 for SCIRXDA operation
     GpioCtrlRegs.GPAMUX2.bit.GPIO29 = 1;   // Configure GPIO29 for SCITXDA operation
 
-
-
+//SCI WORKING MODE
+    SciaRegs.SCICCR.all                 = 0x0007;       // 1 stop bit,  No loopback No parity,8 char bits, async mode, idle-line protocol
+    SciaRegs.SCICTL1.all                = 0x0003;       // enable TX, RX, internal SCICLK, Disable RX ERR, SLEEP, TXWAKE
+    SciaRegs.SCICTL2.all                = 0x0003;
+    SciaRegs.SCICTL2.bit.TXINTENA       = 1;
+    SciaRegs.SCICTL2.bit.RXBKINTENA     = 1;
     switch(cfgSciTblElement.cfgSciParam.sciBaudRate)
     {
         case BAUD_RATE_115200:
@@ -62,10 +64,25 @@ void CFG_SCI_A(CFG_SCI_TBL cfgSciTblElement)
             SciaRegs.SCILBAUD = baudrate & 0x00ff;
             break;
     }
+    SciaRegs.SCICCR.bit.LOOPBKENA       = 0;        // enable loop back
+    SciaRegs.SCICTL1.all                = 0x0023;   // Relinquish SCI from Reset
+
+//FIFO
+    SciaRegs.SCIFFTX.bit.TXFIFOXRESET   = 0;
+    SciaRegs.SCIFFRX.bit.RXFIFORESET    = 0;
+    SciaRegs.SCIFFTX.all                = 0xE028;
+    SciaRegs.SCIFFTX.bit.TXFFIENA       = 0;
+
+    SciaRegs.SCIFFTX.bit.TXFFINTCLR     = 1;
+    SciaRegs.SCIFFRX.all                = 0x2028;
+
+    SciaRegs.SCIFFRX.bit.RXFFIENA       = 1;
+    SciaRegs.SCIFFCT.all                = 0x0;
     EDIS;
 }
 void CFG_SCI_B(CFG_SCI_TBL cfgSciTblElement)
 {
+    Uint16 baudrate = 0;
     EALLOW;
     switch(cfgSciTblElement.cfgSciGpio.rxNum)
     {
@@ -119,10 +136,42 @@ void CFG_SCI_B(CFG_SCI_TBL cfgSciTblElement)
             break;
     }
 
+    ScibRegs.SCICCR.all                 = 0x0007;       // 1 stop bit,  No loopback No parity,8 char bits, async mode, idle-line protocol
+    ScibRegs.SCICTL1.all                = 0x0003;       // enable TX, RX, internal SCICLK, Disable RX ERR, SLEEP, TXWAKE
+    ScibRegs.SCICTL2.all                = 0x0003;
+    ScibRegs.SCICTL2.bit.TXINTENA       = 1;
+    ScibRegs.SCICTL2.bit.RXBKINTENA     = 1;
+    switch(cfgSciTblElement.cfgSciParam.sciBaudRate)
+    {
+        case BAUD_RATE_115200:
+            baudrate = (Uint16)((30000000/115200/8) - 1);
+            ScibRegs.SCIHBAUD = (baudrate & 0xff00) >> 8;
+            ScibRegs.SCILBAUD = baudrate & 0x00ff;
+            break;
+        case BAUD_RATE_9600:
+            baudrate = (Uint16)((30000000/9600/8) - 1);
+            ScibRegs.SCIHBAUD = (baudrate & 0xff00) >> 8;
+            ScibRegs.SCILBAUD = baudrate & 0x00ff;
+            break;
+    }
+    ScibRegs.SCICCR.bit.LOOPBKENA       = 0;        // enable loop back
+    ScibRegs.SCICTL1.all                = 0x0023;   // Relinquish SCI from Reset
+//FIFO
+    ScibRegs.SCIFFTX.bit.TXFIFOXRESET   = 0;
+    ScibRegs.SCIFFRX.bit.RXFIFORESET    = 0;
+    ScibRegs.SCIFFTX.all                = 0xE028;
+    ScibRegs.SCIFFTX.bit.TXFFIENA       = 0;
+
+    ScibRegs.SCIFFTX.bit.TXFFINTCLR     = 1;
+    ScibRegs.SCIFFRX.all                = 0x2028;
+
+    ScibRegs.SCIFFRX.bit.RXFFIENA       = 1;
+    ScibRegs.SCIFFCT.all                = 0x0;
     EDIS;
 }
 void CFG_SCI_C(CFG_SCI_TBL cfgSciTblElement)
 {
+    Uint16 baudrate = 0;
     EALLOW;
     GpioCtrlRegs.GPBPUD.bit.GPIO62 = 0;    // Enable pull-up for GPIO62 (SCIRXDC)
     GpioCtrlRegs.GPBPUD.bit.GPIO63 = 0;    // Enable pull-up for GPIO63 (SCITXDC)
@@ -131,6 +180,38 @@ void CFG_SCI_C(CFG_SCI_TBL cfgSciTblElement)
 
     GpioCtrlRegs.GPBMUX2.bit.GPIO62 = 1;   // Configure GPIO62 for SCIRXDC operation
     GpioCtrlRegs.GPBMUX2.bit.GPIO63 = 1;   // Configure GPIO63 for SCITXDC operation
+
+    ScicRegs.SCICCR.all                 = 0x0007;       // 1 stop bit,  No loopback No parity,8 char bits, async mode, idle-line protocol
+    ScicRegs.SCICTL1.all                = 0x0003;       // enable TX, RX, internal SCICLK, Disable RX ERR, SLEEP, TXWAKE
+    ScicRegs.SCICTL2.all                = 0x0003;
+    ScicRegs.SCICTL2.bit.TXINTENA       = 1;
+    ScicRegs.SCICTL2.bit.RXBKINTENA     = 1;
+    switch(cfgSciTblElement.cfgSciParam.sciBaudRate)
+    {
+        case BAUD_RATE_115200:
+            baudrate = (Uint16)((30000000/115200/8) - 1);
+            ScicRegs.SCIHBAUD = (baudrate & 0xff00) >> 8;
+            ScicRegs.SCILBAUD = baudrate & 0x00ff;
+            break;
+        case BAUD_RATE_9600:
+            baudrate = (Uint16)((30000000/9600/8) - 1);
+            ScicRegs.SCIHBAUD = (baudrate & 0xff00) >> 8;
+            ScicRegs.SCILBAUD = baudrate & 0x00ff;
+            break;
+    }
+    ScicRegs.SCICCR.bit.LOOPBKENA       = 0;        // enable loop back
+    ScicRegs.SCICTL1.all                = 0x0023;   // Relinquish SCI from Reset
+//FIFO
+    ScicRegs.SCIFFTX.bit.TXFIFOXRESET   = 0;
+    ScicRegs.SCIFFRX.bit.RXFIFORESET    = 0;
+    ScicRegs.SCIFFTX.all                = 0xE028;
+    ScicRegs.SCIFFTX.bit.TXFFIENA       = 0;
+
+    ScicRegs.SCIFFTX.bit.TXFFINTCLR     = 1;
+    ScicRegs.SCIFFRX.all                = 0x2028;
+
+    ScicRegs.SCIFFRX.bit.RXFFIENA       = 1;
+    ScicRegs.SCIFFCT.all                = 0x0;
     EDIS;
 }
 
