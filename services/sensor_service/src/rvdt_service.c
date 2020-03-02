161a2e1caefa04110942913903d35347cbd98a17
Simon <liurunyuan0802@163.com>
2020-03-02 19:41:39 +0800

    remove comments

--- services/sensor_service/src/rvdt_service.c
+++ services/sensor_service/src/rvdt_service.c
@@ -3,14 +3,14 @@
 
 int16 Get_RVDT_Position(Uint16 *Resolver_read)
 {
-	int16 rvdt_result_tem1=0;/*ç¬¬ä¸€æ¬¡æ—‹å�˜ä½�ç½®ä¿¡å�·å€¼ï¼Œå�ˆæ³•èŒƒå›´0~4095ï¼Œåˆ�å§‹å€¼ä¸º0ï¼Œå¼•ç”¨å‡½æ•°Rvdt_readï¼Œæ”¹å�˜å‡½æ•°Rvdt_read*/
-	int16 rvdt_result_tem2=0;/*ç¬¬äºŒæ¬¡æ—‹å�˜ä½�ç½®ä¿¡å�·å€¼ï¼Œå�ˆæ³•èŒƒå›´0~4095ï¼Œåˆ�å§‹å€¼ä¸º0ï¼Œå¼•ç”¨å‡½æ•°Rvdt_readï¼Œæ”¹å�˜å‡½æ•°Rvdt_read*/
- 	int16 rvdt_result_tem3=0;/*ç¬¬ä¸‰æ¬¡æ—‹å�˜ä½�ç½®ä¿¡å�·å€¼ï¼Œå�ˆæ³•èŒƒå›´0~4095ï¼Œåˆ�å§‹å€¼ä¸º0ï¼Œå¼•ç”¨å‡½æ•°Rvdt_readï¼Œæ”¹å�˜å‡½æ•°Rvdt_read*/
+	int16 rvdt_result_tem1=0;
+	int16 rvdt_result_tem2=0;
+ 	int16 rvdt_result_tem3=0;
     int16 rvdt_result;
-	rvdt_result_tem1 =*Resolver_read>>4;/*è¿žç»­è¯»ä¸‰æ¬¡æ—‹å�˜ä½�ç½®ä¿¡å�·*/
+	rvdt_result_tem1 =*Resolver_read>>4;
 	rvdt_result_tem2 =*Resolver_read>>4;
 	rvdt_result_tem3 =*Resolver_read>>4;
-	if(rvdt_result_tem1==rvdt_result_tem2)/*å�–ä¸¤æ¬¡ç›¸å�Œçš„å€¼*/
+	if(rvdt_result_tem1==rvdt_result_tem2)
 	{
 		rvdt_result = rvdt_result_tem1;
 	}
