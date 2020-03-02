31e5451cec2cba737d17ae698f7a33b79760b987
Simon <liurunyuan0802@163.com>
2020-03-03 01:03:23 +0800

    fix compile erro

--- platform/pfal/isr/inc/pwm_isr.h
+++ platform/pfal/isr/inc/pwm_isr.h
@@ -3,6 +3,7 @@
 
 #include "DSP2833x_Device.h"     // DSP2833x Headerfile Include File
 #include "DSP2833x_Examples.h"   // DSP2833x Examples Include File
+#include "spwm_service.h"
 
 void PFAL_PWM_ISR(void);
 #endif
