7db5cb45b76250e69692d4ed55360ffdca62cf4f
Simon <liurunyuan0802@163.com>
2020-03-03 16:25:29 +0800

    add macro for system state

--- services/state_machine_service/inc/sys_state_service.h
+++ services/state_machine_service/inc/sys_state_service.h
@@ -1,6 +1,159 @@
 #ifndef SYS_STATE_SERVICE_H_
 #define SYS_STATE_SERVICE_H_
 
+#include "DSP2833x_Device.h"     // DSP2833x Headerfile Include File
+#include "DSP2833x_Examples.h"   // DSP2833x Examples Include File
+#include "prod_defs.h"
+
+typedef struct{
+	Uint16 high8bit : 8;
+	Uint16 low8bit	: 8;
+}VAR16BIT;
+typedef struct _ERROR_16BIT
+{
+	Uint16 software : 1;
+	Uint16 init : 1;
+	Uint16 temperature : 1;
+	Uint16 busVoltage : 1;
+	Uint16 busCurrent : 1;
+	Uint16 hardware : 1;
+	Uint16 g : 1;
+	Uint16 h : 1;
+	Uint16 i : 1;
+	Uint16 j : 1;
+	Uint16 k : 1;
+	Uint16 l : 1;
+	Uint16 m : 1;
+	Uint16 n : 1;
+	Uint16 o : 1;
+	Uint16 p : 1;
+}ERROR_16BIT;
+typedef struct _ALARM_16BIT
+{
+	Uint16 software     : 1;
+	Uint16 init         : 1;
+	Uint16 temperature  : 1;
+	Uint16 busVoltage   : 1;
+	Uint16 busCurrent   : 1;
+	Uint16 hardware     : 1;
+	Uint16 g : 1;
+	Uint16 h : 1;
+	Uint16 i : 1;
+	Uint16 j : 1;
+	Uint16 k : 1;
+	Uint16 l : 1;
+	Uint16 m : 1;
+	Uint16 n : 1;
+	Uint16 o : 1;
+	Uint16 p : 1;
+}ALARM_16BIT;
+
+typedef struct _WARINING_16BIT
+{
+	Uint16 software     : 1;
+	Uint16 init         : 1;
+	Uint16 temperature  : 1;
+	Uint16 busVoltage   : 1;
+	Uint16 busCurrent   : 1;
+	Uint16 hardware     : 1;
+	Uint16 g : 1;
+	Uint16 h : 1;
+	Uint16 i : 1;
+	Uint16 j : 1;
+	Uint16 k : 1;
+	Uint16 l : 1;
+	Uint16 m : 1;
+	Uint16 n : 1;
+	Uint16 o : 1;
+	Uint16 p : 1;
+}WARINING_16BIT;
+
+typedef union
+{
+	Uint16          all;
+	VAR16BIT        data;
+	ERROR_16BIT     bit;
+}SYSERRO;
+
+typedef union
+{
+	Uint16          all;
+	VAR16BIT        data;
+	WARINING_16BIT  bit;
+}SYSWARNING;
+
+typedef union
+{
+	Uint16          all;
+	VAR16BIT        data;
+	ALARM_16BIT     bit;
+}SYSALARM;
+
+
+
+typedef enum _ROTATE_DIRECTION
+{
+    STOP,
+    FORWARD,
+    BACKWARD
+}ROTATE_DIRECTION;
+
+
+
+typedef struct _SYS_STATE_FLAG
+{
+    ROTATE_DIRECTION rotateDirectoin;
+    SYSALARM alarm;
+	SYSWARNING warning;
+	SYSERRO erro;
+    /* data */
+}SYS_STATE_FLAG;
+
+
+#define SOFTWARE_ALARM_MASK                             BIT0
+#define INIT_ALARM_MASK                                 BIT1
+#define TEMPERATURE_ALARM_MASK                          BIT2
+#define BUS_VOLTAGE_ALARM_MASK                          BIT3
+#define BUS_CURRENT_ALARM_MASK                          BIT4
+#define HARDWARE_ALARM_MASK                             BIT5
+
+#define IS_SYS_ENABLE_FORWARD_ROTATE                    (gSysStateFlag.rotateDirectoin == FORWARD)
+#define IS_SYS_ENABLE_BACKWARD_ROTATE                   (gSysStateFlag.rotateDirectoin == BACKWARD)
+#define IS_SYS_ALARM                                    (gSysStateFlag.alarm.all)
+#define IS_SYS_WARNING                                  (gSysStateFlag.warning.all)
+#define CLEAR_SYS_ALARM                                 (gSysStateFlag.alarm.all = 0)
+#define CLEAR_SYS_WARNING                               (gSysStateFlag.warning.all = 0)
+
+
+
+#define SET_SYS_SOFTWARE_ALARM                          (gSysStateFlag.alarm.bit.software = 1)
+#define CLEAR_SYS_SOFTWARE_ALARM                        (gSysStateFlag.alarm.bit.software = 0)
+#define IS_SYS_SOFTWARE_ALARM                           (gSysStateFlag.alarm.bit.software | SOFTWARE_ALARM_MASK)
+
+
+#define SET_SYS_INIT_ALARM                              (gSysStateFlag.alarm.bit.init = 1)
+#define CLEAR_SYS_INIT_ALARM                            (gSysStateFlag.alarm.bit.init = 0)
+#define IS_SYS_INIT_ALARM                               (gSysStateFlag.alarm.bit.init | INIT_ALARM_MASK)
+
+
+#define SET_SYS_TEMPERATURE_ALARM                       (gSysStateFlag.alarm.bit.temperature = 1)
+#define CLEAR_SYS_TEMPERATURE_ALARM                     (gSysStateFlag.alarm.bit.temperature = 0)
+#define IS_SYS_TEMPERATURE_ALARM                        (gSysStateFlag.alarm.bit.temperature | TEMPERATURE_ALARM_MASK)
+
+#define SET_SYS_BUS_VOLTAGE_ALARM                       (gSysStateFlag.alarm.bit.busVoltage = 1)
+#define CLEAR_SYS_BUS_VOLTAGE_ALARM                     (gSysStateFlag.alarm.bit.busVoltage = 0)
+#define IS_SYS_BUS_VOLTAGE_ALARM                        (gSysStateFlag.alarm.bit.busVoltage | BUS_VOLTAGE_ALARM_MASK)
+
+
+#define SET_SYS_BUS_CURRENT_ALARM                       (gSysStateFlag.alarm.bit.busCurrent = 1)
+#define CLEAR_SYS_BUS_CURRENT_ALARM                     (gSysStateFlag.alarm.bit.busCurrent = 0)
+#define IS_SYS_BUS_CURRENT_ALARM                        (gSysStateFlag.alarm.bit.busCurrent | BUS_CURRENT_ALARM_MASK)
+
+
+#define SET_SYS_HARDWARE_ALARM                          (gSysStateFlag.alarm.bit.hardware = 1)
+#define CLEAR_SYS_HARDWARE_ALARM                        (gSysStateFlag.alarm.bit.hardware = 0)
+#define IS_SYS_HARDWARE_ALARM                           (gSysStateFlag.alarm.bit.hardware | HARDWARE_ALARM_MASK)
+
 
 extern void (*Sys_hlstPtr)(void);
 
--- services/state_machine_service/src/sys_state_service.c
+++ services/state_machine_service/src/sys_state_service.c
@@ -1,6 +1,13 @@
 #include "sys_state_service.h"
 
 
+SYS_STATE_FLAG gSysStateFlag = 
+{
+    STOP,
+    0,
+    0
+};
+
 void (*Sys_hlstPtr)(void);
 
 void Sys_hlstInit(void);
@@ -18,36 +25,115 @@ void Sys_hlsAlarm(void);
 #define Sys_chstAlarm()                 Sys_hlstPtr = Sys_hlsAlarm;
 
 
+
 void Sys_hlstInit(void)
 {
-    //system is in the init state;
+    if(IS_SYS_ALARM)
+    {
+        Sys_chstAlarm();
+        return;
+    }
 
-    // check the init success flag
-}
+    if(IS_SYS_WARNING)
+    {
+        Sys_chstWarning();
+        return;
+    }
 
-void Sys_hlstForwardRotate(void)
-{
-    //system is in the init state;
-}
-
-void Sys_hlsBackwardRotate(void)
-{
-    //system is in the init state;
+    Sys_chstStop();
 }
 
 void Sys_hlsStop(void)
 {
-    //system is in the init state;
+    if(IS_SYS_ALARM)
+    {
+        Sys_chstAlarm();
+        return;
+    }
+
+    if(IS_SYS_WARNING)
+    {
+        Sys_chstWarning();
+        return;
+    }
+
+    if(IS_SYS_ENABLE_FORWARD_ROTATE)
+    {
+        Sys_chstForwardRotate();
+        return;
+    }
+
+    if(IS_SYS_ENABLE_BACKWARD_ROTATE)
+    {
+        Sys_chstBackwardRotate();
+        return;
+    }
 }
+
+void Sys_hlstForwardRotate(void)
+{
+    if(IS_SYS_ALARM)
+    {
+        Sys_chstAlarm();
+        return;
+    }
+
+    if(IS_SYS_WARNING)
+    {
+        Sys_chstWarning();
+        return;
+    }
+}
+
+void Sys_hlsBackwardRotate(void)
+{
+    if(IS_SYS_ALARM)
+    {
+        Sys_chstAlarm();
+        return;
+    }
+
+    if(IS_SYS_WARNING)
+    {
+        Sys_chstWarning();
+        return;
+    }
+}
+
+
 
 void Sys_hlsWarning(void)
 {
-    //system is in the init state;
+    if(IS_SYS_ALARM)
+    {
+        Sys_chstAlarm();
+        return;
+    }
+
+    if(IS_SYS_WARNING)
+    {
+        Sys_chstWarning();
+        return;
+    }
+
+    Sys_chstStop();
 }
 
 void Sys_hlsAlarm(void)
 {
-    //system is in the init state;
+    if(IS_SYS_ALARM)
+    {
+        Sys_chstAlarm();
+        return;
+    }
+
+    if(IS_SYS_WARNING)
+    {
+        Sys_chstWarning();
+        return;
+    }
+
+    Sys_chstStop();
 }
 
 
