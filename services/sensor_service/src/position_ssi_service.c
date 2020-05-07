f34652e3de7325241e45a92ad26c15ac1e540ad7
Simon <liurunyuan0802@163.com>
2020-05-07 21:23:10 +0800

    add position sensor SSI service files

---
+++ services/sensor_service/src/position_ssi_service.c
@@ -0,0 +1,57 @@
+#include "position_ssi_service.h"
+
+POSITION_SSI_PARA gPositionSSIPara = {0};
+
+void PositionSSIService(void)
+{
+    static int count = 0;
+    static int subWait = 0;
+    Uint32 tmp = 0;
+    static Uint32 gSSIDATA = 0;
+
+    if(count == 0)
+    {
+        GpioDataRegs.GPBCLEAR.bit.GPIO63 = 1;
+        count++;
+        gSSIDATA = 0;
+        
+        return;
+    }
+
+    if(count == 1)
+    {
+        GpioDataRegs.GPBSET.bit.GPIO63 = 1;
+        count++;
+        return;
+    }
+
+    if(count == 33)
+    {
+        GpioDataRegs.GPBSET.bit.GPIO63 = 1;
+
+        if(subWait++ > gPositionSSIPara.subWaitTimes)
+        {
+            gPositionSSIPara.currentPos = gSSIDATA >> 1;
+            subWait = 0;
+            count = 0;
+        }
+        return;
+    }
+
+    if((count % 2) == 0)
+    {
+        tmp = GpioDataRegs.GPBDAT.bit.GPIO62;
+        gSSIDATA |= (tmp << (17 - (count/2)));
+        GpioDataRegs.GPBCLEAR.bit.GPIO63 = 1;
+        count++;
+        return;
+    }
+
+    if((count % 2) == 1)
+    {
+        GpioDataRegs.GPBSET.bit.GPIO63 = 1;
+        count++;
+        return;
+    }   
+}
+
