055c8979c54a87381bb537aadb453e934506213e
LRY <liurunyuan0802@163.com>
2020-08-24 05:22:12 -0900

    add log file

---
+++ services/log_service/inc/log.h
@@ -0,0 +1,8 @@
+#ifndef LOG_H_
+#define LOG_H_
+
+#include "DSP2833x_Device.h"     // DSP2833x Headerfile Include File
+#include "DSP2833x_Examples.h"   // DSP2833x Examples Include File
+
+#endif
+
